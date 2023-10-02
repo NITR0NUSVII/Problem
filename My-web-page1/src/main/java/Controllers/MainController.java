024d1718255980043b3884cf7405175d66be64b1
NITR0NUSVII <143868237+NITR0NUSVII@users.noreply.github.com>
2023-10-02 20:25:27 +0300

    Add files via upload

---
+++ My-web-page1/src/main/java/Controllers/MainController.java
@@ -0,0 +1,17 @@
+package Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("title", "Main page");
+        return "home";
+    }
+
+}
---
+++ My-web-page1/src/main/java/com/nitronus/My/web/page1/MyWebPage1Application.java
@@ -0,0 +1,13 @@
+package com.nitronus.My.web.page1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWebPage1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyWebPage1Application.class, args);
+	}
+
+}
